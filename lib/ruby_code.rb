2a7f410130529853bd1e8fcba45cf405d0745428
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2026-03-14 01:35:00 -0600

    BUGFIX: fix the cover printing twice bug

--- lib/ruby_code.rb
+++ lib/ruby_code.rb
@@ -6,6 +6,6 @@ require_relative "ruby_code/initializer"
 # Main module for the RubyCode gem
 module RubyCode
   class Error < StandardError; end
-  # Your code goes here...
-  Initializer.new.print_cover
+
+  Initializer.new
 end
