4f571c2311c0b0ab43f9df1894e5a33b22229d40
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2026-04-15 00:52:22 -0600

    Fix gemfile lock file for CI

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## [Unreleased]
 
-## [0.1.0] - 2026-03-14
+## [0.1.0] - 2026-04-15
 
 - Initial release
+
+## [0.1.1] - 2026-04-15
+
+- Fix CI workflow
--- lib/ruby_code/version.rb
+++ lib/ruby_code/version.rb
@@ -2,7 +2,7 @@
 
 # This module contains the version of the RubyCode gem
 module RubyCode
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 
   def self.gem_version
     Gem::Version.new(VERSION).freeze
